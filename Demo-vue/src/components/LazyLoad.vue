11f5f38a768139bfeeabd89e0f3b34b936f3ed21
jinyong <x-jinyong@hotmail.com>
2019-07-16 20:59:50 +0800

    addpic

--- Demo-vue/src/components/LazyLoad.vue
+++ Demo-vue/src/components/LazyLoad.vue
@@ -14,7 +14,11 @@ export default {
       picList: [
         'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562650577938&di=d83dc20192090337c3ae1ac4af4a6a76&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201808%2F15%2F20180815104043_bocaq.jpg',
         'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562650577938&di=248279fa82cce97bca99196b7d08d4e7&imgtype=0&src=http%3A%2F%2Fimg.redocn.com%2Fsheji%2F20150731%2Fliubianxingkejiganshipinsucai_4753838.jpg',
-        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562650577937&di=14a13f763c01986e28d36784becd9718&imgtype=0&src=http%3A%2F%2Fimg.yanj.cn%2Fstore%2Fgoods%2F4824%2F4824_e7bc5d96ccc3c9e27ba662d47299f435.jpg_max.jpg'
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562650577937&di=14a13f763c01986e28d36784becd9718&imgtype=0&src=http%3A%2F%2Fimg.yanj.cn%2Fstore%2Fgoods%2F4824%2F4824_e7bc5d96ccc3c9e27ba662d47299f435.jpg_max.jpg',
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1563291749929&di=a9cc75ca948e8d6802fe991b78284bbe&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F0f939e0e12d267c543daf515d0a85b15d3fc40cc5248a-so7Q7E_fw658',
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1563291802494&di=10ef64d3b00e238d45ba0ca37ed09b2b&imgtype=0&src=http%3A%2F%2Fimages.ofweek.com%2FUpload%2FNews%2F2018-11%2F05%2Fruanxueke%2F1541381519780092303.jpg',
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1563291846310&di=459392ebff1d3a0b81fef8b286e1e9d9&imgtype=0&src=http%3A%2F%2Fpic.58pic.com%2F58pic%2F15%2F02%2F65%2F67d58PICkMZ_1024.jpg',
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1563291866300&di=28ad6b3812124785e631a82986dc5e30&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20170923%2F8eaf9c4654874b2dbc2154780b97c9df.jpeg'
       ]
     }
   }
--- Demo-vue/src/components/Table.vue
+++ Demo-vue/src/components/Table.vue
@@ -1,371 +1,88 @@
 <template>
-  <div>
-    <a-table :columns="myColumns" :dataSource="myData" bordered>
-      <span slot="expandedRowRender" slot-scope="record,index">
-        <div class="clearfix" style="margin-bottom: 20px;">
-          <a-table
-            :columns="myColumnsInner"
-            :dataSource="record.data"
-            :pagination="false"
-            :showHeader="false"
-            :rowKey="items => items.configId"
-          ></a-table>
-        </div>
-      </span>
-    </a-table>
-  </div>
+  <a-table :dataSource="data">
+    <a-table-column-group>
+      <span slot="title" style="color: #1890ff">Name</span>
+      <a-table-column
+        dataIndex="firstName"
+        key="firstName"
+      >
+        <span slot="title" style="color: #1890ff">First Name</span>
+      </a-table-column>
+      <a-table-column
+        title="Last Name"
+        dataIndex="lastName"
+        key="lastName"
+      />
+    </a-table-column-group>
+    <a-table-column
+      title="Age"
+      dataIndex="age"
+      key="age"
+    />
+    <a-table-column
+      title="Address"
+      dataIndex="address"
+      key="address"
+    />
+    <a-table-column
+      title="Tags"
+      dataIndex="tags"
+      key="tags"
+    >
+      <template slot-scope="tags">
+        <span>
+          <a-tag v-for="tag in tags" color="blue" :key="tag">{{tag}}</a-tag>
+        </span>
+      </template>
+    </a-table-column>
+    <a-table-column
+      title="Action"
+      key="action"
+    >
+      <template slot-scope="text, record">
+        <span>
+          <a href="javascript:;">Action 一 {{record.firstName}}</a>
+          <a-divider type="vertical" />
+          <a href="javascript:;">Delete</a>
+        </span>
+      </template>
+    </a-table-column>
+  </a-table>
 </template>
 
 <script>
-// In the fifth row, other columns are merged into first column
-// by setting it's colSpan to be 0
-const renderContent = (value, row, index) => {
-  const obj = {
-    children: value,
-    attrs: {}
-  };
-  if (value == "一组") {
-    obj.attrs.colSpan = 6;
-  }
-  return obj;
-};
-
-const data = [
-  {
-    key: "1",
-    name: "John Brown",
-    age: 32,
-    tel: "0571-22098909",
-    phone: 18889898989,
-    address: "New York No. 1 Lake Park"
-  },
-  {
-    key: "2",
-    name: "Jim Green",
-    tel: "0571-22098333",
-    phone: 18889898888,
-    age: 42,
-    address: "London No. 1 Lake Park"
-  },
-  {
-    key: "3",
-    name: "Joe Black",
-    age: 32,
-    tel: "0575-22098909",
-    phone: 18900010002,
-    address: "Sidney No. 1 Lake Park"
-  },
-  {
-    key: "4",
-    name: "Jim Red",
-    age: 18,
-    tel: "0575-22098909",
-    phone: 18900010002,
-    address: "London No. 2 Lake Park"
-  },
-  {
-    key: "5",
-    name: "Jake White",
-    age: 18,
-    tel: "0575-22098909",
-    phone: 18900010002,
-    address: "Dublin No. 2 Lake Park"
-  }
-];
-const myData = [
-  {
-    group: "一组",
-    data: [
-      {
-        key: 1,
-        chassName: "公共管理",
-        classCode: "AE322FS",
-        competent: "经管学院",
-        credit: "3",
-        period: "72",
-        classNumber: "3"
-      },
-      {
-        key: 2,
-        chassName: "公共管理",
-        classCode: "AE322FS",
-        competent: "经管学院",
-        credit: "3",
-        period: "72",
-        classNumber: "3"
-      },
-      {
-        key: 3,
-        chassName: "公共管理",
-        classCode: "AE322FS",
-        competent: "经管学院",
-        credit: "3",
-        period: "72",
-        classNumber: "3"
-      },
-      {
-        key: 4,
-        chassName: "公共管理",
-        classCode: "AE322FS",
-        competent: "经管学院",
-        credit: "3",
-        period: "72",
-        classNumber: "3"
-      }
-    ]
-  },
-  {
-    group: "二组",
-    data: [
-      {
-        key: 5,
-        chassName: "公共管理",
-        classCode: "AE322FS",
-        competent: "经管学院",
-        credit: "3",
-        period: "72",
-        classNumber: "3"
-      },
-      {
-        key: 6,
-        chassName: "公共管理",
-        classCode: "AE322FS",
-        competent: "经管学院",
-        credit: "3",
-        period: "72",
-        classNumber: "3"
-      },
-      {
-        key: 7,
-        chassName: "公共管理",
-        classCode: "AE322FS",
-        competent: "经管学院",
-        credit: "3",
-        period: "72",
-        classNumber: "3"
-      }
-    ]
-  },
-  {
-    group: "三组",
-    data: [
-      {
-        key: 8,
-        chassName: "公共管理",
-        classCode: "AE322FS",
-        competent: "经管学院",
-        credit: "3",
-        period: "72",
-        classNumber: "3"
-      },
-      {
-        key: 9,
-        chassName: "公共管理",
-        classCode: "AE322FS",
-        competent: "经管学院",
-        credit: "3",
-        period: "72",
-        classNumber: "3"
-      },
-      {
-        key: 10,
-        chassName: "公共管理",
-        classCode: "AE322FS",
-        competent: "经管学院",
-        credit: "3",
-        period: "72",
-        classNumber: "3"
-      }
-    ]
-  }
-];
+const data = [{
+  key: '1',
+  firstName: 'John',
+  lastName: 'Brown',
+  age: 32,
+  address: 'New York No. 1 Lake Park',
+  tags: ['nice', 'developer'],
+}, {
+  key: '2',
+  firstName: 'Jim',
+  lastName: 'Green',
+  age: 42,
+  address: 'London No. 1 Lake Park',
+  tags: ['loser'],
+}, {
+  key: '3',
+  firstName: 'Joe',
+  lastName: 'Black',
+  age: 32,
+  address: 'Sidney No. 1 Lake Park',
+  tags: ['cool', 'teacher'],
+}];
 
 export default {
   data() {
-    const columns = [
-      {
-        title: "Name",
-        dataIndex: "name",
-        customRender: (text, row, index) => {
-          if (index < 4) {
-            return <a href="javascript:;">{text}</a>;
-          }
-          return {
-            children: <a href="javascript:;">{text}</a>,
-            attrs: {
-              colSpan: 5
-            }
-          };
-        }
-      },
-      {
-        title: "Age",
-        dataIndex: "age",
-        customRender: renderContent
-      },
-      {
-        title: "Home phone",
-        colSpan: 2,
-        dataIndex: "tel",
-        customRender: (value, row, index) => {
-          const obj = {
-            children: value,
-            attrs: {}
-          };
-          if (index === 2) {
-            obj.attrs.rowSpan = 2;
-          }
-          // These two are merged into above cell
-          if (index === 3) {
-            obj.attrs.rowSpan = 0;
-          }
-          if (index === 4) {
-            obj.attrs.colSpan = 0;
-          }
-          return obj;
-        }
-      },
-      {
-        title: "Phone",
-        colSpan: 0,
-        dataIndex: "phone",
-        customRender: renderContent
-      },
-      {
-        title: "Address",
-        dataIndex: "address",
-        customRender: renderContent
-      }
-    ];
-    const myColumns = [
-      {
-        title: "课程名称",
-        dataIndex: "group",
-        width: '20%',
-        customRender: (value, row, index) => {
-          return {
-            children: value,
-            attrs: {
-              colSpan: 6
-            }
-          };
-        }
-      },
-      {
-        title: "课程代码",
-        dataIndex: 2,
-        width: '20%',
-        customRender: (value, row, index) => {
-          return {
-            attrs: {
-              colSpan: 0
-            }
-          };
-        }
-      },
-      {
-        title: "主管部门",
-        dataIndex: 3,
-        width: '20%',
-        customRender: (value, row, index) => {
-          return {
-            attrs: {
-              colSpan: 0
-            }
-          };
-        }
-      },
-      {
-        title: "学分",
-        dataindex: 4,
-        width: '12%',
-        customRender: (value, row, index) => {
-          return {
-            attrs: {
-              colSpan: 0
-            }
-          };
-        }
-      },
-      {
-        title: "总学时",
-        dataIndex: 5,
-        width: '12%',
-        customRender: (value, row, index) => {
-          return {
-            attrs: {
-              colSpan: 0
-            }
-          };
-        }
-      },
-      {
-        title: "教学班号",
-        dataindex: 6,
-        width: '12%',
-        customRender: (value, row, index) => {
-          return {
-            attrs: {
-              colSpan: 0
-            }
-          };
-        }
-      }
-    ];
-    const myColumnsInner = [
-      {
-        title: "课程名称",
-        dataIndex: "chassName",
-        width: '20%',
-      },
-      {
-        title: "课程代码",
-        dataIndex: 'classCode',
-        width: '20%',
-      },
-      {
-        title: "主管部门",
-        dataIndex: 'competent',
-        width: '20%',
-      },
-      {
-        title: "学分",
-        dataIndex: 'credit',
-        width: '12%',
-      },
-      {
-        title: "总学时",
-        dataIndex: 'period',
-        width: '12%',
-      },
-      {
-        title: "教学班号",
-        dataIndex: 'classNumber',
-        width: '12%',
-      }
-    ];
     return {
       data,
-      columns,
-      myColumns,
-      myData,
-      myColumnsInner
-    };
+    }
   }
-};
+}
 </script>
 
-<style lang="scss" scoped>
-  /deep/ .ant-table-expanded-row-level-1 {
-    td {
-      border: 0
-    }
-  } 
-  /deep/ .ant-table-bordered {
-    .ant-table-tbody {
-      td {
-        border-right: 0
-      }
-    }
-  } 
+<style  scoped>
+  
 </style>
